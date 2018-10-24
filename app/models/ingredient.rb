f1a9cf17c4a0bd41428bbabc193f0697bbac3681
Joseph Arias <arias.joseph.a@gmail.com>
2018-10-24 11:38:21 -0400

    Don't use

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,17 @@ class Ingredient < ActiveRecord::Base
   has_many :burger_ingredients
   has_many :burgers, through: :burger_ingredients
 
-
+  def createPrice(input)
+    case input
+    when "Lettuce"
+      price = 0.20
+    when "Tomato"
+      price = 0.10
+    when "Cheese"
+      price = 0.30
+    when "Onion"
+      price = 0.5
+    end
+  end
   
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,5 @@
 require_relative '../config/environment'
 
-
-
-  
   p "Hi, Welome to Flatiron Burger!"
   p "Make your own Burger"
   p "What is the name of the order?"
@@ -13,8 +10,11 @@ require_relative '../config/environment'
   new_burger.menu_output
   ing_input = gets.chomp.capitalize
 
-  # comparing and finding a match instance of an ingredient
-  new_ingredient = Ingredient.find_by(name: ing_input)
+  new_ingredeint = Ingredient.createPrice(ing_input)
+  p new_ingredeint
+
+  comparing and finding a match instance of an ingredient
+  new_ingredient = Ingredient.create(name: ing_input)
   p new_ingredient
 
   # creating a burger ingredient
@@ -27,18 +27,17 @@ require_relative '../config/environment'
 
   answer_input = gets.chomp.downcase
 
-  # if answer_input == "yes"
-  #   new_burger.menu_output
-  #   ing_input = gets.chomp.capitalize
-  #   new_ingredient = Ingredient.find_by(name: ing_input)
-  #   p new_ingredient
+  if answer_input == "yes"
+    new_burger.menu_output
+    ing_input = gets.chomp.capitalize
+    new_ingredient = Ingredient.find_by(name: ing_input)
+    p new_ingredient
     
-  #   new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: new_ingredient)
-  #   binding.pry
-  #   p "Current Ingredients: #{new_burger.output_ingredient}"
-  #   p new_burger.price
+    new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: new_ingredient)
+    p "Current Ingredients: #{new_burger.output_ingredient}"
+    p new_burger.price
     
-  # end
+  end
 
 
 
@@ -49,34 +48,34 @@ require_relative '../config/environment'
 
 
 
-# ing_input = new_burger.menu_output
-# new_ingredient = Ingredient.find_by(name: ing_input)
-# new_burger_ingredient = BurgerIngredient.create(burger:new_burger, ingredient:new_ingredient)
-# new_burger.output_ingredient
-# new_burger.price
+ing_input = new_burger.menu_output
+new_ingredient = Ingredient.find_by(name: ing_input)
+new_burger_ingredient = BurgerIngredient.create(burger:new_burger, ingredient:new_ingredient)
+new_burger.output_ingredient
+new_burger.price
 
 
-# if answer_input == "yes"
-#   ing_input = new_burger.menu_output
-#   new_ingredient = Ingredient.find_by(name: ing_input)
-#   n_b_ingredient = BurgerIngredient.create(burger:new_burger, ingredient:new_ingredient)
-#   p new_burger.output_ingredient
-#   p new_burger.price
-# elsif answer_input == "no"
-#   puts "Here your Order!"
-# else
-#   puts "Not a known command, Please try again"
-# end
+if answer_input == "yes"
+  ing_input = new_burger.menu_output
+  new_ingredient = Ingredient.find_by(name: ing_input)
+  n_b_ingredient = BurgerIngredient.create(burger:new_burger, ingredient:new_ingredient)
+  p new_burger.output_ingredient
+  p new_burger.price
+elsif answer_input == "no"
+  puts "Here your Order!"
+else
+  puts "Not a known command, Please try again"
+end
 
-# # p new_burger.ingredients.map{|ing|ing.name}
-# # new_total_price = new_burger.ingredients.map{|ing| ing.price}.reduce(:+)
+p new_burger.ingredients.map{|ing|ing.name}
+new_total_price = new_burger.ingredients.map{|ing| ing.price}.reduce(:+)
 
-# # p "Your total price is #{new_total_price}"
-# # # first we need to create burger_ingredient
-# # #   the method burger.ingredients will return all ingredients in that specific burger
+p "Your total price is #{new_total_price}"
+first we need to create burger_ingredient
+  the method burger.ingredients will return all ingredients in that specific burger
 
-# # # new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: typeAnIngredient)
-# # # p new_burger_ingredient
+new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: typeAnIngredient)
+p new_burger_ingredient
 
 
 
