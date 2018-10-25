abe7c3fbb479b2e22ccdeb95c9b544f5c228f56e
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2018-10-25 12:02:45 -0400

    it loops if the he wants to order something differnt

---
+++ bin/helper.rb
@@ -0,0 +1,263 @@
+def helper
+
+  new_burger = Burger.create(name: name_input)
+  new_burger.menu_output
+  ing_input = gets.chomp.capitalize
+
+  # while Ingredient.all.map{|ing|ing.name}.include?ing_input
+
+  # comparing and finding a match instance of an ingredient
+  new_ingredient = Ingredient.find_by(name: ing_input)
+  if new_ingredient
+  p new_ingredient
+
+  # creating a burger ingredient
+  new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: new_ingredient)
+  puts "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+  p new_burger.price
+  # binding.pry
+  else
+  p "Sorry, we don't have that."
+  end
+  puts "Any more ingredients?"
+  puts "Yes or No"
+
+  answer_input = gets.chomp.downcase
+  while answer_input != "yes" && answer_input != "no"
+    puts "Please enter yes or no!"
+    answer_input = gets.chomp.downcase
+  end
+
+  while answer_input == "yes"
+    new_burger.menu_output
+    ing_input = gets.chomp.capitalize
+    # comparing and finding a match instance of an ingredient
+    new_ingredient = Ingredient.find_by(name: ing_input)
+
+    if new_ingredient
+      p new_ingredient
+
+
+      # creating a burger ingredient
+      new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: new_ingredient)
+
+      new_burger = Burger.all.find(new_burger.id)
+
+      # binding.pry
+
+      p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+      p new_burger.price
+    else
+      p "Sorry, we don't have that."
+    end
+
+    puts "Any more ingredients?"
+    puts "Yes or No"
+    # binding.pry
+    answer_input = gets.chomp.downcase
+    while answer_input != "yes" && answer_input != "no"
+      puts "Please enter yes or no!"
+      answer_input = gets.chomp.downcase
+    end
+  end
+
+  if answer_input == "no"
+    puts "Do you want to update the order?"
+    puts "Yes or No"
+    order_input = gets.chomp.downcase
+    while order_input != "yes" && order_input != "no"
+      puts "Please enter yes or no!"
+      order_input = gets.chomp.downcase
+    end
+    case order_input
+    when "no"
+      puts '
+       _    _               _                                              _           _
+      | |  | |             ( )                                            | |         | |
+      | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
+      |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
+      | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
+      |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
+                                     __/ |
+                                    |___/
+      '
+      puts new_burger.total_price
+      puts  '
+         _....----"""----...._
+      .-"  o    o    o    o   "-.
+     /  o    o    o         o    \
+    /     o      o   o     o    o \
+  _|   o   o    o      o  o     o  |_
+  / `""-----.................-----""` \
+  \___________________________________/
+   \~`-`.__.`-~`._.~`-`~.-~.__.~`-`/
+    \                             /
+     `-._______________________.-"
+
+      '
+    when "yes"
+      puts "Do you want to ADD or DELETE an ingredient?"
+      puts "Type no if neither"
+      ingredient_input = gets.chomp.downcase
+      while ingredient_input != "add" && ingredient_input != "delete" && ingredient_input != "no"
+        puts "Please enter add or delete or no."
+        ingredient_input = gets.chomp.downcase
+      end
+      case ingredient_input
+      when "add"
+        new_burger.menu_output
+        ing_input = gets.chomp.capitalize
+        # comparing and finding a match instance of an ingredient
+        new_ingredient = Ingredient.find_by(name: ing_input)
+        p new_ingredient
+
+        # creating a burger ingredient
+        new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: new_ingredient)
+        new_burger = Burger.all.find(new_burger.id)
+        p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+        p new_burger.price
+
+        puts "Any more ingredients?"
+        puts "Yes or No"
+        answer_input = gets.chomp.downcase
+        while answer_input != "yes" && answer_input != "no"
+            puts "Please enter yes or no!"
+            answer_input = gets.chomp.downcase
+          end
+        while answer_input == "yes"
+          new_burger.menu_output
+          ing_input = gets.chomp.capitalize
+          # comparing and finding a match instance of an ingredient
+          new_ingredient = Ingredient.find_by(name: ing_input)
+          p new_ingredient
+
+          # creating a burger ingredient
+          new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: new_ingredient)
+          new_burger = Burger.all.find(new_burger.id)
+          p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+          p new_burger.price
+
+          puts "Any more ingredients?"
+          puts "Yes or No"
+          answer_input = gets.chomp.downcase
+        end
+        if answer_input == "no"
+          puts '
+
+       _    _               _                                              _           _
+      | |  | |             ( )                                            | |         | |
+      | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
+      |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
+      | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
+      |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
+                                     __/ |
+                                    |___/
+
+      '
+
+      puts new_burger.total_price
+          puts  '
+         _....----"""----...._
+      .-"  o    o    o    o   "-.
+     /  o    o    o         o    \
+    /     o      o   o     o    o \
+  _|   o   o    o      o  o     o  |_
+  / `""-----.................-----""` \
+  \___________________________________/
+   \~`-`.__.`-~`._.~`-`~.-~.__.~`-`/
+    \                             /
+     `-._______________________.-"
+
+      '
+        end
+      when "delete"
+        new_burger.menu_output
+        p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+        ing_input = gets.chomp.capitalize
+        # comparing and finding a match instance of an ingredient
+        find_ingredient = Ingredient.find_by(name: ing_input)
+        new_burger.delete_ingredient(find_ingredient)
+        p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+
+        puts "Delete any more ingredients?"
+        puts "Yes or No"
+        answer_input = gets.chomp.downcase
+
+        while answer_input == "yes"
+          new_burger.menu_output
+          p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+          ing_input = gets.chomp.capitalize
+
+          # comparing and finding a match instance of an ingredient
+          find_ingredient = Ingredient.find_by(name: ing_input)
+          new_burger.delete_ingredient(find_ingredient)
+          p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+
+          # If ingredients don't have any ingredients return order
+          if new_burger.ingredients.empty? == true
+            return puts "Here's your Order!"
+          end
+
+          puts "Delete any more ingredients?"
+          puts "Yes or No"
+          answer_input = gets.chomp.downcase
+        end
+        if answer_input == "no"
+          puts '
+
+       _    _               _                                              _           _
+      | |  | |             ( )                                            | |         | |
+      | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
+      |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
+      | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
+      |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
+                                     __/ |
+                                    |___/
+
+      '
+          puts  '
+         _....----"""----...._
+      .-"  o    o    o    o   "-.
+     /  o    o    o         o    \
+    /     o      o   o     o    o \
+  _|   o   o    o      o  o     o  |_
+  / `""-----.................-----""` \
+  \___________________________________/
+   \~`-`.__.`-~`._.~`-`~.-~.__.~`-`/
+    \                             /
+     `-._______________________.-"
+
+      '
+        end
+
+
+      when "no"
+        puts '
+
+       _    _               _                                              _           _
+      | |  | |             ( )                                            | |         | |
+      | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
+      |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
+      | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
+      |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
+                                     __/ |
+                                    |___/
+
+      '
+        puts  '
+         _....----"""----...._
+      .-"  o    o    o    o   "-.
+     /  o    o    o         o    \
+    /     o      o   o     o    o \
+  _|   o   o    o      o  o     o  |_
+  / `""-----.................-----""` \
+  \___________________________________/
+   \~`-`.__.`-~`._.~`-`~.-~.__.~`-`/
+    \                             /
+     `-._______________________.-"
+
+      '
+      end
+    end
+  end
+end
--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,5 @@
 require_relative '../config/environment'
+require_relative '../helper.rb'
 require "pry"
 
 puts  '
@@ -28,9 +29,279 @@ puts '
   p "Build your own Burger"
   p "What is the name for the order?"
   name_input = gets.chomp
-  new_burger = Burger.create(name: name_input)
-  p new_burger
+  new_burger = Burger.find_by(name: name_input)
+  if new_burger
+  p "You recently built a burger with #{new_burger.output_ingredient.join("||")}, would you like to reorder now?"
+  p "please type yes or no."
 
+  respose_input = gets.chomp.downcase
+    if respose_input == "yes"
+      puts new_burger.price
+    else
+      p "Build your own Burger**************************"
+      new_burger = Burger.create(name: name_input)
+      new_burger.menu_output
+      ing_input = gets.chomp.capitalize
+
+      # while Ingredient.all.map{|ing|ing.name}.include?ing_input
+
+      # comparing and finding a match instance of an ingredient
+      new_ingredient = Ingredient.find_by(name: ing_input)
+      if new_ingredient
+      p new_ingredient
+
+      # creating a burger ingredient
+      new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: new_ingredient)
+      puts "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+      p new_burger.price
+      # binding.pry
+      else
+      p "Sorry, we don't have that."
+      end
+      puts "Any more ingredients?"
+      puts "Yes or No"
+
+      answer_input = gets.chomp.downcase
+      while answer_input != "yes" && answer_input != "no"
+        puts "Please enter yes or no!"
+        answer_input = gets.chomp.downcase
+      end
+
+      while answer_input == "yes"
+        new_burger.menu_output
+        ing_input = gets.chomp.capitalize
+        # comparing and finding a match instance of an ingredient
+        new_ingredient = Ingredient.find_by(name: ing_input)
+
+        if new_ingredient
+          p new_ingredient
+
+
+          # creating a burger ingredient
+          new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: new_ingredient)
+
+          new_burger = Burger.all.find(new_burger.id)
+
+          # binding.pry
+
+          p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+          p new_burger.price
+        else
+          p "Sorry, we don't have that."
+        end
+
+        puts "Any more ingredients?"
+        puts "Yes or No"
+        # binding.pry
+        answer_input = gets.chomp.downcase
+        while answer_input != "yes" && answer_input != "no"
+          puts "Please enter yes or no!"
+          answer_input = gets.chomp.downcase
+        end
+      end
+
+      if answer_input == "no"
+        puts "Do you want to update the order?"
+        puts "Yes or No"
+        order_input = gets.chomp.downcase
+        while order_input != "yes" && order_input != "no"
+          puts "Please enter yes or no!"
+          order_input = gets.chomp.downcase
+        end
+        case order_input
+        when "no"
+          puts '
+           _    _               _                                              _           _
+          | |  | |             ( )                                            | |         | |
+          | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
+          |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
+          | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
+          |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
+                                         __/ |
+                                        |___/
+          '
+          puts new_burger.total_price
+          puts  '
+             _....----"""----...._
+          .-"  o    o    o    o   "-.
+         /  o    o    o         o    \
+        /     o      o   o     o    o \
+      _|   o   o    o      o  o     o  |_
+     / `""-----.................-----""` \
+     \___________________________________/
+       \~`-`.__.`-~`._.~`-`~.-~.__.~`-`/
+        \                             /
+         `-._______________________.-"
+
+          '
+        when "yes"
+          puts "Do you want to ADD or DELETE an ingredient?"
+          puts "Type no if neither"
+          ingredient_input = gets.chomp.downcase
+          while ingredient_input != "add" && ingredient_input != "delete" && ingredient_input != "no"
+            puts "Please enter add or delete or no."
+            ingredient_input = gets.chomp.downcase
+          end
+          case ingredient_input
+          when "add"
+            new_burger.menu_output
+            ing_input = gets.chomp.capitalize
+            # comparing and finding a match instance of an ingredient
+            new_ingredient = Ingredient.find_by(name: ing_input)
+            p new_ingredient
+
+            # creating a burger ingredient
+            new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: new_ingredient)
+            new_burger = Burger.all.find(new_burger.id)
+            p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+            p new_burger.price
+
+            puts "Any more ingredients?"
+            puts "Yes or No"
+            answer_input = gets.chomp.downcase
+            while answer_input != "yes" && answer_input != "no"
+                puts "Please enter yes or no!"
+                answer_input = gets.chomp.downcase
+              end
+            while answer_input == "yes"
+              new_burger.menu_output
+              ing_input = gets.chomp.capitalize
+              # comparing and finding a match instance of an ingredient
+              new_ingredient = Ingredient.find_by(name: ing_input)
+              p new_ingredient
+
+              # creating a burger ingredient
+              new_burger_ingredient = BurgerIngredient.create(burger: new_burger, ingredient: new_ingredient)
+              new_burger = Burger.all.find(new_burger.id)
+              p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+              p new_burger.price
+
+              puts "Any more ingredients?"
+              puts "Yes or No"
+              answer_input = gets.chomp.downcase
+            end
+            if answer_input == "no"
+              puts '
+
+           _    _               _                                              _           _
+          | |  | |             ( )                                            | |         | |
+          | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
+          |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
+          | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
+          |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
+                                         __/ |
+                                        |___/
+
+          '
+
+          puts new_burger.total_price
+              puts  '
+             _....----"""----...._
+          .-"  o    o    o    o   "-.
+         /  o    o    o         o    \
+        /     o      o   o     o    o \
+      _|   o   o    o      o  o     o  |_
+     / `""-----.................-----""` \
+     \___________________________________/
+       \~`-`.__.`-~`._.~`-`~.-~.__.~`-`/
+        \                             /
+         `-._______________________.-"
+
+          '
+            end
+          when "delete"
+            new_burger.menu_output
+            p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+            ing_input = gets.chomp.capitalize
+            # comparing and finding a match instance of an ingredient
+            find_ingredient = Ingredient.find_by(name: ing_input)
+            new_burger.delete_ingredient(find_ingredient)
+            p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+
+            puts "Delete any more ingredients?"
+            puts "Yes or No"
+            answer_input = gets.chomp.downcase
+
+            while answer_input == "yes"
+              new_burger.menu_output
+              p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+              ing_input = gets.chomp.capitalize
+
+              # comparing and finding a match instance of an ingredient
+              find_ingredient = Ingredient.find_by(name: ing_input)
+              new_burger.delete_ingredient(find_ingredient)
+              p "CURRENT INGREDIENTS: #{new_burger.output_ingredient}"
+
+              # If ingredients don't have any ingredients return order
+              if new_burger.ingredients.empty? == true
+                return puts "Here's your Order!"
+              end
+
+              puts "Delete any more ingredients?"
+              puts "Yes or No"
+              answer_input = gets.chomp.downcase
+            end
+            if answer_input == "no"
+              puts '
+
+           _    _               _                                              _           _
+          | |  | |             ( )                                            | |         | |
+          | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
+          |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
+          | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
+          |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
+                                         __/ |
+                                        |___/
+
+          '
+              puts  '
+             _....----"""----...._
+          .-"  o    o    o    o   "-.
+         /  o    o    o         o    \
+        /     o      o   o     o    o \
+      _|   o   o    o      o  o     o  |_
+     / `""-----.................-----""` \
+     \___________________________________/
+       \~`-`.__.`-~`._.~`-`~.-~.__.~`-`/
+        \                             /
+         `-._______________________.-"
+
+          '
+            end
+
+
+          when "no"
+            puts '
+
+           _    _               _                                              _           _
+          | |  | |             ( )                                            | |         | |
+          | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
+          |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
+          | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
+          |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
+                                         __/ |
+                                        |___/
+
+          '
+            puts  '
+             _....----"""----...._
+          .-"  o    o    o    o   "-.
+         /  o    o    o         o    \
+        /     o      o   o     o    o \
+      _|   o   o    o      o  o     o  |_
+     / `""-----.................-----""` \
+     \___________________________________/
+       \~`-`.__.`-~`._.~`-`~.-~.__.~`-`/
+        \                             /
+         `-._______________________.-"
+
+          '
+          end
+        end
+      end
+    end
+  else
+  new_burger = Burger.create(name: name_input)
   new_burger.menu_output
   ing_input = gets.chomp.capitalize
 
@@ -101,15 +372,15 @@ puts '
     end
     case order_input
     when "no"
-      puts ' 
-       _    _               _                                              _           _ 
+      puts '
+       _    _               _                                              _           _
       | |  | |             ( )                                            | |         | |
       | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
       |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
       | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
       |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
-                                     __/ |                                               
-                                    |___/                                               
+                                     __/ |
+                                    |___/
       '
       puts new_burger.total_price
       puts  '
@@ -172,17 +443,17 @@ puts '
           answer_input = gets.chomp.downcase
         end
         if answer_input == "no"
-          puts ' 
-      
-       _    _               _                                              _           _ 
+          puts '
+
+       _    _               _                                              _           _
       | |  | |             ( )                                            | |         | |
       | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
       |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
       | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
       |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
-                                     __/ |                                               
-                                    |___/                                               
-      
+                                     __/ |
+                                    |___/
+
       '
 
       puts new_burger.total_price
@@ -233,17 +504,17 @@ puts '
           answer_input = gets.chomp.downcase
         end
         if answer_input == "no"
-          puts ' 
-      
-       _    _               _                                              _           _ 
+          puts '
+
+       _    _               _                                              _           _
       | |  | |             ( )                                            | |         | |
       | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
       |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
       | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
       |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
-                                     __/ |                                               
-                                    |___/                                               
-      
+                                     __/ |
+                                    |___/
+
       '
           puts  '
          _....----"""----...._
@@ -262,17 +533,17 @@ puts '
 
 
       when "no"
-        puts ' 
-      
-       _    _               _                                              _           _ 
+        puts '
+
+       _    _               _                                              _           _
       | |  | |             ( )                                            | |         | |
       | |__| | ___ _ __ ___|/ ___   _   _  ___  _   _ _ __    ___  _ __ __| | ___ _ __| |
       |  __  |/ _ \  __/ _ \ / __| | | | |/ _ \| | | |  __|  / _ \|  __/ _` |/ _ \  __| |
       | |  | |  __/ | |  __/ \__ \ | |_| | (_) | |_| | |    | (_) | | | (_| |  __/ |  |_|
       |_|  |_|\___|_|  \___| |___/  \__, |\___/ \__,_|_|     \___/|_|  \__,_|\___|_|  ( )
-                                     __/ |                                               
-                                    |___/                                               
-      
+                                     __/ |
+                                    |___/
+
       '
         puts  '
          _....----"""----...._
@@ -290,3 +561,4 @@ puts '
       end
     end
   end
+end
