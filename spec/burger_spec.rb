f0bb237f17cfd81f1df1f2f82af12681ce7c9866
Joseph Arias <arias.joseph.a@gmail.com>
2020-02-12 15:14:41 -0500

    added quotes to testing array

--- spec/burger_spec.rb
+++ spec/burger_spec.rb
@@ -1,9 +1,10 @@
+
 describe '#output_ingredient' do
-  it "puts ingredient of a burger" do
+  it "puts ingredients of a burger" do
 
     @burger = Burger.find_by(name:"pollo_loco")
 
-    expect(@burger.output_ingredient).to eq(["lettuce, tomatoes, chicken, Szechuan sauce"])
+    expect(@burger.output_ingredient).to eq(["lettuce", "tomatoes", "chicken", "szechuan sauce"])
 
   end
 end
