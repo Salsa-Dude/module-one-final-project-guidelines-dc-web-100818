72679ae06f7bcc78495c601b4cdf3f3007d57b6f
Joseph Arias <arias.joseph.a@gmail.com>
2020-02-12 15:02:05 -0500

    remove merge lines

--- spec/burger_spec.rb
+++ spec/burger_spec.rb
@@ -2,10 +2,10 @@ describe '#output_ingredient' do
   it "puts ingredient of a burger" do
 
     @burger = Burger.find_by(name:"cacrv")
-<<<<<<< HEAD
+
     expect(@burger.output_ingredient).to eq(["Onio","Onion", "Onion", "Onion"])
-=======
+
     expect(@burger.output_ingredient).to eq(["Onion""Onion", "Onion", "Onion"])
->>>>>>> new_dev
+
   end
 end
